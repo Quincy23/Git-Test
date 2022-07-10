d6336ec9aefd81a9ef1897d02be4ea5553bd20cb
Quincy23 <97208890+Quincy23@users.noreply.github.com>
2022-07-10 15:42:36 +0800

    pull commit

--- src/main/java/edu/atlingshi/gitTest.java
+++ src/main/java/edu/atlingshi/gitTest.java
@@ -10,5 +10,6 @@ public class gitTest {
         System.out.println("Hello Git3!");
         System.out.println("Hello Master!");
         System.out.println("push commit!");
+        System.out.println("pull commit!");
     }
 }
