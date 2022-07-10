fb310d2351311de12c6f603467537d741846cbc3
Quincy <quincychen78@163.com>
2022-07-10 15:16:55 +0800

    merge first commit

--- src/main/java/edu/atlingshi/gitTest.java
+++ src/main/java/edu/atlingshi/gitTest.java
@@ -3,7 +3,8 @@ package edu.atlingshi;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello Git2");
-        System.out.println("Hello Git3");
+        System.out.println("Hello Git2!");
+        System.out.println("Hello Git3!");
+        System.out.println("Hello Master!");
     }
 }
